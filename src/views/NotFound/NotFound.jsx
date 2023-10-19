04d3a274d39aa1763109501dd279f48a1e2ab268
Cristian Valencia G <117423462+cvalenciapatagonia@users.noreply.github.com>
2023-10-19 19:11:50 -0300

    Update NotFound.jsx
    Limpiando codigo de recordatorios

--- src/views/NotFound/NotFound.jsx
+++ src/views/NotFound/NotFound.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Asegúrate de importar Link
+import { Link } from "react-router-dom";
 import "../NotFound/NotFound.css";
 
 export default function NotFound() {
   return (
-    <div className="error-container"> {/* Corrige el atributo className */}
+    <div className="error-container"> -
       <div>
         <img className="error-img" src="src/assets/img/Error404.png" alt="." />
       </div>
