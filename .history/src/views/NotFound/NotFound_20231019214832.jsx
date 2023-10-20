7b429445bc3b6ef02f81e0a78d6a81754342c34c
Tu Nombre <cvalencia.patagonia@gmail.com>
2023-10-19 21:56:45 -0300

    reemplazo ruta local por raw en Error404.png para compatibilidad con gh-pages

--- .history/src/views/NotFound/NotFound_20231019214832.jsx
+++ .history/src/views/NotFound/NotFound_20231019214832.jsx
@@ -6,10 +6,7 @@ export default function NotFound() {
   return (
     <div className="error-container"> 
       <div>
-        {/* Ruta local original comentada para reemplazarla por la utilizacion de ruta raw 👇*/}
-        {/* <img className="error-img" src="src/assets/img/Error404.png" alt="." /> */ } 
-        <img className="error-img" src="https://raw.githubusercontent.com/cvalenciapatagonia/Desafio-prueba-4-React-II/ff9d6e9736a55eea251a1d83cac2a42148046cb4/src/assets/img/Error404.png" alt="." /> {/*Ruta Raw compatible con gh-pages*/}
-
+        <img className="error-img" src="src/assets/img/Error404.png" alt="." />
       </div>
       <div>
         <Link to="/Desafio-prueba-4-React-II/">
--- .history/src/views/NotFound/NotFound_20231019215240.jsx
+++ .history/src/views/NotFound/NotFound_20231019215240.jsx
@@ -6,9 +6,9 @@ export default function NotFound() {
   return (
     <div className="error-container"> 
       <div>
-        {/* Ruta local original comentada para reemplazarla por la utilizacion de ruta raw 👇*/}
+        {/* Ruta local original comentada para la utilizacion de ruta raw 👇*/}
         {/* <img className="error-img" src="src/assets/img/Error404.png" alt="." /> */ } 
-        <img className="error-img" src="https://raw.githubusercontent.com/cvalenciapatagonia/Desafio-prueba-4-React-II/ff9d6e9736a55eea251a1d83cac2a42148046cb4/src/assets/img/Error404.png" alt="." /> {/*Ruta Raw compatible con gh-pages*/}
+        <img className="error-img" src="https://raw.githubusercontent.com/cvalenciapatagonia/Desafio-prueba-4-React-II/ff9d6e9736a55eea251a1d83cac2a42148046cb4/src/assets/img/Error404.png" alt="." /> 
 
       </div>
       <div>
