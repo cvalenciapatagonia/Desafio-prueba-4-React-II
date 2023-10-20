ff9d6e9736a55eea251a1d83cac2a42148046cb4
Tu Nombre <cvalencia.patagonia@gmail.com>
2023-10-19 21:24:53 -0300

    merge para pull y posterior correccion

---
+++ .history/src/views/NotFound/NotFound_20231019212359.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom"; 
+import "../NotFound/NotFound.css";
+
+export default function NotFound() {
+  return (
+    <div className="error-container"> 
+      <div>
+        <img className="error-img" src="src/assets/img/Error404.png" alt="." />
+      </div>
+      <div>
+        <Link to="/Desafio-prueba-4-React-II/home">
+          <button type="button" className="btn btn-dark btn-error btn-lg">
+            Volver a la pagina principal
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
